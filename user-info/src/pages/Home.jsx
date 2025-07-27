fdc852d632b3b5024eed771a730aafb299449351
jnakht <jisan.cse.cu@gmail.com>
2025-07-27 22:28:55 +0600

    module 20.10 done

--- user-info/src/pages/Home.jsx
+++ user-info/src/pages/Home.jsx
@@ -27,6 +27,12 @@ const Home = () => {
                     ...state,
                     [action.payload.field]: action.payload.value
                 }
+            case "RESET": 
+                return initialState;
+            case "CLEAR": 
+                 return {
+                    [action.payload.field]: "",
+                 }
             default: 
                 return state;
         }
@@ -51,47 +57,64 @@ const Home = () => {
         // console.log({name, email, phone});
         console.log(userInfo);
     }
+    const handleReset = () => {
+        dispatch({
+            type: "RESET"
+        })
+    }
+    const handleClear = (fieldName) => {
+        dispatch({
+           type: "CLEAR", 
+           payload: {
+                field: fieldName
+           }
+        })
+    }
 
     return (
-        <div>
+        <div className='flex flex-col justify-center items-center w-[50vw] h-[50vh] border mx-auto my-auto'>
             <h3>Welcome To Our App</h3>
             <form onSubmit={handleSubmit}>
                 <div>
                     <label className='text-lg'>Name</label>
                     <input
-                        className='bg-amber-300'
+                        className='bg-amber-300 ml-4 h-[40px]'
                         type="text"
                         name="name"
                         id="name"
                         value={userInfo.name}
                         onChange={handleOnChange}
                     />
+                    <button onClick={() => handleClear('name')} className='btn'>clear</button>
                 </div>
 
-                <div>
+                <div  className='my-4'>
                     <label className='text-lg'>Email</label>
                     <input
-                        className='bg-amber-300'
+                        className='bg-amber-300 ml-4 h-[40px]'
                         type="email"
                         name="email"
                         id="email"
                         value={userInfo.email}
                         onChange={handleOnChange}
                     />
+                    <button onClick={() => handleClear('email')} className='btn'>clear</button>
                 </div>
 
                 <div>
                     <label className='text-lg'>Phone</label>
                     <input
-                        className='bg-amber-300'
+                        className='bg-amber-300 ml-4 h-[40px]'
                         type="text"
                         name="phone"
                         id="phone"
                         value={userInfo.phone}
                         onChange={handleOnChange}
                     />
+                    <button onClick={() => handleClear('phone')} className='btn'>clear</button>
                 </div>
-                <button>Submit</button>
+                <button onClick={handleReset} className='btn'>Reset</button>
+                <button className='btn'>Submit</button>
             </form>
         </div>
     );
