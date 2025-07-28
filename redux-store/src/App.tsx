38236996b93b79e6efb780b08ef865d5799a7984
jnakht <jisan.cse.cu@gmail.com>
2025-07-28 11:59:01 +0600

    module 21.10 done

--- redux-store/src/App.tsx
+++ redux-store/src/App.tsx
@@ -1,11 +1,24 @@
 
-import { useDispatch } from 'react-redux';
+import { useDispatch, useSelector } from 'react-redux';
 import './App.css'
 import { decrement, increment } from './redux/features/counter/counterSlice';
+import type { RootState } from './redux/store';
+import { useAppDispatch, useAppSelector } from './redux/hooks';
+
 
 function App() {
   
-  const dispatch = useDispatch();
+  // const dispatch = useDispatch(); 
+  const dispatch = useAppDispatch();
+
+
+
+  // const counter = useSelector((state) => state.counter);
+  // console.log(counter); //obj ,, to get => counter.count
+  // const { count } = useSelector((state : RootState) => state.counter)
+  const { count } = useAppSelector((state) => state.counter)
+
+
   const handleIncrement = () => {
     dispatch(increment());
   }
@@ -17,7 +30,7 @@ function App() {
     <div>
         <h3>Counter With Redux</h3>
         <button onClick={handleIncrement}>Increment</button>
-        <div>0</div>
+        <div>{count}</div>
         <button onClick={handleDecrement}>Decrement</button>
     </div>
   )
