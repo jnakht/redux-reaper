cf56eb23dceed74d88484bdfe7f85ce4fe045577
jnakht <jisan.cse.cu@gmail.com>
2025-07-29 17:08:53 +0600

    module 22.7 done

--- redux-todo-app/src/App.tsx
+++ redux-todo-app/src/App.tsx
@@ -3,16 +3,25 @@ import { Outlet } from 'react-router'
 import './App.css'
 import { Button } from './components/ui/button'
 import { Link } from 'react-router-dom'
+import { ModeToggle } from './components/ui/mode-toggle'
 
 
 function App() {
- 
+
 
   return (
     <>
-      <Button>This is App Component</Button>
-      <Button><Link to="Tasks">Tasks</Link></Button>
-      <Button><Link to="Users">Users</Link></Button>
+      <div className='flex justify-between'>
+        <div>
+          <Button>This is App Component</Button>
+          <Button><Link to="Tasks">Tasks</Link></Button>
+          <Button><Link to="Users">Users</Link></Button>
+        </div>
+
+        <div>
+          <ModeToggle></ModeToggle>
+        </div>
+      </div>
       <Outlet></Outlet>
     </>
   )
--- redux-todo-app/src/main.tsx
+++ redux-todo-app/src/main.tsx
@@ -6,12 +6,15 @@ import { Provider } from 'react-redux'
 import { store } from './redux/store.ts'
 import { RouterProvider } from 'react-router'
 import router from './routes/index.tsx'
+import { ThemeProvider } from './providers/theme-provider.tsx'
 
 createRoot(document.getElementById('root')!).render(
   <StrictMode>
-    <Provider store={store}>
+   <ThemeProvider defaultTheme='dark' storageKey='vite-ui-theme'>
+     <Provider store={store}>
       <RouterProvider router={router}>
       </RouterProvider>
     </Provider>
+   </ThemeProvider>
   </StrictMode>,
 )
