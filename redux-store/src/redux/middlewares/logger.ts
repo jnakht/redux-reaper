cc4cbe9c982c37e2010da9b45e21b6cf513cfaa3
jnakht <jisan.cse.cu@gmail.com>
2025-07-29 11:21:59 +0600

    logger middleware recap

--- redux-store/src/redux/middlewares/logger.ts
+++ redux-store/src/redux/middlewares/logger.ts
@@ -1,12 +1,26 @@
 
 
+// const logger = (state) => (next) => (action) => {
+//     // console.log("getState ", state.getState());
+//     // console.log("action: ", action);
+//     // return next(action);
+
+
+
+//     console.group(action.type);
+//     console.info("prev state: ", state.getState());
+//     const result = next(action);
+//     console.info("next state: ", state.getState());
+//     console.groupEnd();
+//     return result;
+// }
+
+// export default logger;
+
+
+
+
 const logger = (state) => (next) => (action) => {
-    // console.log("getState ", state.getState());
-    // console.log("action: ", action);
-    // return next(action);
-
-
-
     console.group(action.type);
     console.info("prev state: ", state.getState());
     const result = next(action);
