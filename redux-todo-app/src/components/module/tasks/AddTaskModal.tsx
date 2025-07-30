7b883eea11e737ec0ef99c86093ab31a01f8d339
jnakht <jisan.cse.cu@gmail.com>
2025-07-30 17:00:50 +0600

    redux-todo-app - priority selector added in form

--- redux-todo-app/src/components/module/tasks/AddTaskModal.tsx
+++ redux-todo-app/src/components/module/tasks/AddTaskModal.tsx
@@ -12,6 +12,7 @@ import {
 import { Form, FormControl, FormField, FormItem, FormLabel } from "@/components/ui/form"
 import { Input } from "@/components/ui/input"
 import { Label } from "@/components/ui/label"
+import { Select, SelectContent, SelectItem, SelectTrigger, SelectValue } from "@/components/ui/select"
 import { Textarea } from "@/components/ui/textarea"
 import { useForm } from "react-hook-form"
 
@@ -31,51 +32,74 @@ export function AddTaskModal() {
         <DialogContent className="sm:max-w-[425px]">
           <DialogHeader>
             <DialogTitle>Add Task</DialogTitle>
-            
-          </DialogHeader>
-         
-          <Form {...form}>
-              <form onSubmit={form.handleSubmit(onSubmit)}>
-            
-            <DialogDescription>Title</DialogDescription>
-            <FormField
-              control={form.control}
-              name="title"
-              render={({field}) => (
-                
-                  <FormItem>
-                    <FormLabel />
-                    <FormControl>
-                      <Input {...field} value={field.value || ""}/>
-                    </FormControl>
-                  </FormItem>
-               
-              )}
-            />
-            <DialogDescription>Description</DialogDescription>
-            <FormField
-              control={form.control}
-              name="description"
-              render={({field}) => (
-                
-                  <FormItem>
-                    <FormLabel />
-                    <FormControl>
-                      <Textarea {...field} value={field.value || ""}/>
-                    </FormControl>
-                  </FormItem>
-               
-              )}
-            />
 
-            
-            <DialogFooter>
-              <DialogClose asChild>
-                <Button variant="outline">Cancel</Button>
-              </DialogClose>
-              <Button type="submit">Save changes</Button>
-            </DialogFooter>
-             </form>
+          </DialogHeader>
+
+          <Form {...form}>
+            <form onSubmit={form.handleSubmit(onSubmit)}>
+
+              {/* <DialogDescription>Title</DialogDescription> */}
+              <FormField
+                control={form.control}
+                name="title"
+                render={({ field }) => (
+
+                  <FormItem>
+                     <FormLabel>Title</FormLabel>
+                    <FormControl>
+                      <Input {...field} value={field.value || ""} />
+                    </FormControl>
+                  </FormItem>
+
+                )}
+              />
+              {/* <DialogDescription>Description</DialogDescription> */}
+              <FormField
+                control={form.control}
+                name="description"
+                render={({ field }) => (
+
+                  <FormItem>
+                    <FormLabel>Description</FormLabel>
+                    <FormControl>
+                      <Textarea {...field} value={field.value || ""} />
+                    </FormControl>
+                  </FormItem>
+
+                )}
+              />
+
+
+              <FormField 
+                control={form.control}
+                name="priority"
+                render={({ field }) => (
+                  <FormItem>
+                    <FormLabel>Priority</FormLabel>
+                    <Select onValueChange={field.onChange} defaultValue={field.value}>
+                      <FormControl>
+                        <SelectTrigger className="w-full">
+                          <SelectValue placeholder="Select a priority" />
+                        </SelectTrigger>
+                      </FormControl>
+                      <SelectContent>
+                        <SelectItem value="Low">Low</SelectItem>
+                        <SelectItem value="Medium">Medium</SelectItem>
+                        <SelectItem value="High">High</SelectItem>
+                      </SelectContent>
+                    </Select>
+                  </FormItem>
+                )}
+              />
+
+
+              <DialogFooter>
+                <DialogClose asChild>
+                  <Button variant="outline">Cancel</Button>
+                </DialogClose>
+                <Button type="submit">Save changes</Button>
+              </DialogFooter>
+            </form>
           </Form>
         </DialogContent>
       </form>
