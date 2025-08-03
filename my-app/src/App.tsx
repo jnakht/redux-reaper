dc507b259e137d89a855af4f7e115fe010a19b1e
jnakht <jisan.cse.cu@gmail.com>
2025-08-04 05:04:40 +0600

    module 22.7 recap

--- my-app/src/App.tsx
+++ my-app/src/App.tsx
@@ -6,34 +6,39 @@ import { decrement, increment } from './redux/features/counter/counterSlice';
 import Tasks from './pages/Tasks.tsx';
 import User from './pages/User.tsx';
 import { useAppDispatch, useAppSelector } from './redux/hooks.ts';
+import { ModeToggle } from './components/ui/mode-toggle.tsx';
 
 function App() {
   const dispatch = useAppDispatch();
   const { count } = useAppSelector((state) => state.counter);
   // console.log("This is the count: ", count);
 
-  const incrementHandler = (value : number) => {
+  const incrementHandler = (value: number) => {
     dispatch(increment(value));
   }
   const decrementHandler = () => {
     dispatch(decrement());
   }
   const handleBlockButton = () => {
-    dispatch({type: "BLOCK_ME"});
+    dispatch({ type: "BLOCK_ME" });
   }
 
   return (
     <>
-      <div>
-        <Link to="tasks"><Button>Tasks</Button></Link>
-        <Link to="users"><Button>Users</Button></Link>
+      <div className='flex justify-between max-w-[80%] mx-auto'>
+        <div>
+          <Link to="tasks"><Button>Tasks</Button></Link>
+          <Link to="users"><Button>Users</Button></Link>
+        </div>
+        <ModeToggle></ModeToggle>
+      </div>
         {/* <Button onClick={ () => incrementHandler(5) }>Increment By 5</Button>
         <Button onClick={ () => incrementHandler(1) }>Increment</Button>
         <h3>{ count }</h3>
         <Button onClick={decrementHandler}>Decrement</Button>
         <Button onClick={handleBlockButton}>Block Me</Button> */}
         <Outlet></Outlet>
-      </div>
+      
     </>
   )
 }
